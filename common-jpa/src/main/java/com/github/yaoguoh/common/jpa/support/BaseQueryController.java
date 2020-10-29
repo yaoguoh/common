954dbc052ba3f1af1d835c8a731358085bec33f4
WYG <yaoguohh@gmail.com>
2020-10-29 17:20:25 +0800

    UPDATE   - swagger3.0 param date type update

--- common-jpa/src/main/java/com/github/yaoguoh/common/jpa/support/BaseQueryController.java
+++ common-jpa/src/main/java/com/github/yaoguoh/common/jpa/support/BaseQueryController.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Example;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import springfox.documentation.annotations.ApiIgnore;
